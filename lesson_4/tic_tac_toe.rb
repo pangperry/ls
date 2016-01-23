8bca09e1ebb75bb31c37e60481f8d692cd66f913
Mark Perry <mark.j.perry11@gmail.com>
2016-01-23 01:07:33 -0800

    Add tic tac toe
    *still includes optional code in comments
    *includes bonus features 1-3

---
+++ lesson_4/tic_tac_toe.rb
@@ -0,0 +1,186 @@
+# q1.rb
+require 'pry'
+
+WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
+                [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # cols
+                [[1, 5, 9], [3, 5, 7]] # diagnols
+INITIAL_MARKER =  ' '.freeze
+PLAYER_MARKER = 'X'.freeze
+COMPUTER_MARKER = 'O'.freeze
+
+def prompt(msg)
+  puts "=> #{msg}"
+end
+
+# rubocop: disable Metrics/AbcSize
+def display_board(brd)
+  system 'clear'
+  puts "You're an #{PLAYER_MARKER}"
+  puts ""
+  puts "     |     |"
+  puts "  #{brd[1]}  |  #{brd[2]}  |   #{brd[3]}"
+  puts "     |     |"
+  puts "-----|-----|-----"
+  puts "     |     |"
+  puts "  #{brd[4]}  |  #{brd[5]}  |   #{brd[6]}"
+  puts "     |     |"
+  puts "-----|-----|-----"
+  puts "     |     |"
+  puts "  #{brd[7]}  |  #{brd[8]}  |   #{brd[9]}"
+  puts "     |     |"
+  puts ""
+end
+# rubocop: enable Metrics/AbcSize
+
+def initialize_player_score
+  @player_score = 0
+end
+
+def initialize_computer_score
+  @computer_score = 0
+end
+
+def initialize_board
+  new_board = {}
+  (1..9).each { |num| new_board[num] = INITIAL_MARKER }
+  new_board
+end
+
+def empty_squares(brd)
+  brd.keys.select { |num| brd[num] == INITIAL_MARKER }
+end
+
+def joinor(arr, punctuation = ', ', conjunction = 'or ')
+  arr.join(punctuation).insert(-2, conjunction)
+end
+# other possible solution:
+=begin
+def joinor(arr, delimiter, word='or')
+    arr[-1] = "#{word} #{arr.last}" if arr.size > 1
+      arr.join(delimiter)
+end
+=end
+
+def detect_threat(brd)
+  WINNING_LINES.each do |line|
+    if brd.values_at(*line).count(PLAYER_MARKER) == 2 &&
+       rd.values_at(*line).count(INITIAL_MARKER) == 1
+
+      square = line.find { |num| brd[num] == INITIAL_MARKER }
+      return square
+    else
+      next
+    end
+  end
+  nil
+end
+
+def player_places_piece!(brd)
+  square = ''
+  loop do
+    # prompt "Choose a position to place the peace: #{empty_squares(brd).join(', ')}"
+    prompt "Choose a position to place the peace: #{joinor(empty_squares(brd), '; ')}"
+    square = gets.chomp.to_i
+    break if empty_squares(brd).include?(square)
+    prompt "Sorry, that's not a valid choice"
+  end
+
+  brd[square] = PLAYER_MARKER
+end
+
+def computer_places_piece!(brd)
+  square =
+    if detect_threat(brd)
+      detect_threat(brd)
+    else
+      empty_squares(brd).sample
+    end
+
+  brd[square] = COMPUTER_MARKER
+end
+
+def board_full?(brd)
+  empty_squares(brd).empty?
+end
+
+def someone_won?(brd)
+  !!detect_winner(brd)
+end
+
+def keep_score(winner)
+  winner == 'Player' ? @player_score += 1 : @computer_score += 1
+end
+
+def win_five?(score)
+  score == 5
+end
+
+# rubocop: disable Metrics/MethodLength
+def detect_winner(brd)
+  WINNING_LINES.each do |line|
+=begin
+    if brd[line[0]] == PLAYER_MARKER &&
+       brd[line[1]] == PLAYER_MARKER &&
+       brd[line[2]] == PLAYER_MARKER
+      return 'Player'
+    elsif brd[line[0]] == COMPUTER_MARKER &&
+          brd[line[1]] == COMPUTER_MARKER &&
+          brd[line[2]] == COMPUTER_MARKER
+      return 'Computer'
+    end
+    if brd.values_at(line[0], line[1], line[2]).count(PLAYER_MARKER) == 3
+      return 'Player'
+    elsif brd.values_at(line[0], line[1], line[2]).count(COMPUTER_MARKER) == 3
+      return 'Computer'
+    end
+=end
+    if brd.values_at(*line).count(PLAYER_MARKER) == 3
+      return 'Player'
+    elsif brd.values_at(*line).count(COMPUTER_MARKER) == 3
+      return 'Computer'
+    end
+  end
+  nil
+end
+# rubocop: enable Metrics/MethodLength
+
+initialize_player_score
+initialize_computer_score
+
+loop do
+  board = initialize_board
+
+  loop do
+    display_board(board)
+    player_places_piece!(board)
+    break if someone_won?(board) || board_full?(board)
+    computer_places_piece!(board)
+    display_board(board)
+    break if someone_won?(board) || board_full?(board)
+  end
+
+  display_board(board)
+
+  if someone_won?(board)
+    prompt "#{detect_winner(board)} won!"
+    keep_score(detect_winner(board))
+  else
+    prompt "It's a tie!"
+  end
+
+  if win_five?(@player_score)
+    prompt("Player wins 5! Game over!")
+    break
+  elsif win_five?(@computer_score)
+    prompt("Computer wins 5! Game over!")
+    break
+  else
+    puts "Current score is Player: #{@player_score}, Computer: #{@computer_score}"
+  end
+
+  prompt "Play again?(y or n)"
+  answer = gets.chomp
+  break unless answer.downcase.start_with?('y')
+end
+
+prompt "Thanks for playing tic tac toe. Goodbye!"
