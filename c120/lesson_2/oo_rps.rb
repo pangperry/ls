97f3873d335fa81f421eccfabd50da2d40d08126
Mark Perry <mark.j.perry11@gmail.com>
2016-02-08 13:40:43 -0800

    add design choice 2 for oo_rps.rb

--- c120/lesson_2/oo_rps.rb
+++ c120/lesson_2/oo_rps.rb
@@ -1,3 +1,39 @@
+class Move
+  VALUES = ['rock', 'paper', 'scissors'].freeze
+
+  def initialize(value)
+    @value = value
+  end
+
+  def scissors?
+    @value == 'scissors'
+  end
+
+  def rock?
+    @value == 'rock'
+  end
+
+  def paper?
+    @value == 'paper'
+  end
+
+  def >(other_move)
+    (rock?     && other_move.scissors?) ||
+      (paper?    && other_move.rock?)   ||
+      (scissors? && other_move.paper?)
+  end
+
+  def <(other_move)
+    (rock?     && other_move.paper?)      ||
+      (paper?    && other_move.scissors?) ||
+      (scissors? && other_move.rock?)
+  end
+
+  def to_s
+    @value
+  end
+end
+
 class Player
   attr_accessor :move, :name
 
@@ -8,14 +44,14 @@ end
 
 class Human < Player
   def set_name
-      n = ''
-      loop do
-        puts "What's your name?"
-        n = gets.chomp
-        break unless n.empty?
-        puts "Sorry, must enter a value."
-      end
-      self.name = n
+    n = ''
+    loop do
+      puts "What's your name?"
+      n = gets.chomp
+      break unless n.empty?
+      puts "Sorry, must enter a value."
+    end
+    self.name = n
   end
 
   def choose
@@ -23,20 +59,20 @@ class Human < Player
     loop do
       puts "Please choose rock, paper, or scissors:"
       choice = gets.chomp
-      break if ['rock', 'paper', 'scissors'].include? choice
+      break if Move::VALUES.include? choice
       puts "Sorry, invalid choice."
     end
-    self.move = choice
+    self.move = Move.new(choice)
   end
 end
 
 class Computer < Player
   def set_name
-      self.name = ['R2D2', 'Hal', 'Chappie', 'Sonny', 'Number 5'].sample
+    self.name = ['R2D2', 'Hal', 'Chappie', 'Sonny', 'Number 5'].sample
   end
 
   def choose
-    self.move = ['rock', 'paper', 'scissors'].sample
+    self.move = Move.new(Move::VALUES.sample)
   end
 end
 
@@ -57,23 +93,18 @@ class Game
     puts "Thanks for palying Rock, Paper, Scissors! Goodbye!"
   end
 
-  def display_winner
+  def display_moves
     puts "#{human.name} chose #{human.move}."
     puts "#{computer.name} chose #{computer.move}"
+  end
 
-    case human.move
-    when 'rock'
-      puts "It's a tie!" if computer.move == 'rock'
-      puts "#{human.name} won!" if computer.move == 'scissors'
-      puts "#{computer.name} Computer won!" if computer.move == 'paper'
-    when 'paper'
-      puts "It's a tie!" if computer.move == 'paper'
-      puts "#{human.name} won!" if computer.move == 'rock'
-      puts "#{computer.name} Computer won!" if computer.move == 'scissors'
-    when 'scissors'
-      puts "It's a tie!" if computer.move == 'scissors'
-      puts "#{human.name} won!" if computer.move == 'paper'
-      puts "#{computer.name} Computer won!" if computer.move == 'rock'
+  def display_winner
+    if human.move > computer.move
+      puts "#{human.name} won!"
+    elsif human.move < computer.move
+      puts "#{computer.name} won!"
+    else
+      puts "It's a tie!"
     end
   end
 
@@ -87,7 +118,7 @@ class Game
     end
 
     return true if answer == 'y'
-    return false
+    false
   end
 
   def play
@@ -95,10 +126,11 @@ class Game
     loop do
       human.choose
       computer.choose
+      display_moves
       display_winner
       break unless play_again?
     end
-      display_goodbye_message
+    display_goodbye_message
   end
 end
 
